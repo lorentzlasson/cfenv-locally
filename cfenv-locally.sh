ff55374523976bdb04849f6592e776221773f178
Lorentz Lasson <lorentz.lasson@se.ibm.com>
2015-10-11 20:45:42 +0200

    fix: structed as expected by cf

--- cfenv-locally.sh
+++ cfenv-locally.sh
@@ -1,7 +1,8 @@
 APP_NAME=$1
 ENVS="$(cf env $APP_NAME)"
 # Delete all before "System-Provided"
-ENVS=$(sed 's/^.*System-Provided: //' <<< $ENVS)
+ENVS=$(sed 's/^.*{ "VCAP_SERVICES": //' <<< $ENVS)
 # Delete all after "User-Provided"
-ENVS=$(sed s/"{ \"VCAP_APPLICATION\":.*"/""/ <<< $ENVS)
+ENVS=$(sed s/"} { \"VCAP_APPLICATION\":.*"/""/ <<< $ENVS)
 export VCAP_SERVICES=$ENVS
+
